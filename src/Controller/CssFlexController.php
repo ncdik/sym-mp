8903fd5895288b5f9604bd3c135f29d0e95e3e65
Mikhail Zhukov <mikhail.zhukov@sibers.com>
2018-11-09 18:34:21 +0700

    add css/flex

--- src/Controller/CssFlexController.php
+++ src/Controller/CssFlexController.php
@@ -12,8 +12,17 @@ class CssFlexController extends AbstractController
      */
     public function index()
     {
+        for ($i=0;$i<15;$i++) {
+            $r = dechex(rand(0x90, 0xee));
+            $g = dechex(rand(0x90, 0xee));
+            $b = dechex(rand(0x90, 0xee));
+            if(strlen($r)<2) $r = '0'.$r;
+            if(strlen($g)<2) $g = '0'.$g;
+            if(strlen($b)<2) $b = '0'.$b;
+            $colors[] = '#'.$r.$g.$b;
+        }
         return $this->render('css_flex/index.html.twig', [
-            'controller_name' => 'CssFlexController',
+            'colors' => $colors,
         ]);
     }
 }
